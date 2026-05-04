5e74321644d5ac76045ce4ebf3900c152c123592
mohamed hisham <mohamedhis36@gmail.com>
2026-05-04 17:51:04 +0300

    added icons and fonts

---
+++ lib/assets.dart
@@ -0,0 +1,9 @@
+class Assets {
+  static const String settingsIcon = 'assets/icon/44-setting.svg';
+  static const String bellIcon = 'assets/icon/76-notification.svg';
+  static const String exploreIcon = 'assets/icon/42-explore.svg';
+  static const String scanIcon = 'assets/icon/21-san.svg';
+  static const String profileIcon = 'assets/icon/11-profile.svg';
+  static const String chartIcon = 'assets/icon/5-chart-1.svg';
+  static const String homeIcon = 'assets/icon/2-home-11.svg';
+}
